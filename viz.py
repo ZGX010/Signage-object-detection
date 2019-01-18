0e7cc82b68e2b0f36833cef25d27c752b4a97d33
ZGX010 <36026086+ZGX010@users.noreply.github.com>
2019-01-18 16:54:41 +0800

    Update viz.py

--- viz.py
+++ viz.py
@@ -1,19 +1,22 @@
 from PIL import Image
 import sys
+from PIL import Image
+import sys
 
 def blend_two_images():
     for i in range(3):
-        i = str(i).zfill(6)
-        img_add = '/home/cxx/labelmemaster/viz/'+ i +'_image.png'
+        i = str(i)    #.zfill(6)
+        print(i)
+        img_add = '/home/zgx010/Desktop/blendimgtest/'+ i +'.jpg'
         img1 = Image.open(img_add)
         img1 = img1.convert('RGBA')
-        label_add = '/home/cxx/labelmemaster/viz/'+ i +'_prediction.png'
+        label_add = '/home/zgx010/Desktop/blendimgtest/'+ i +'_1.png'
         img2 = Image.open(label_add)
         img2 = img2.convert('RGBA')
 
         img = Image.blend(img1, img2, 0.3)
         #img.show()
-        img.save('/home/cxx/labelmemaster/viz/result/blend'+ i +'.png')
+        img.save('/home/zgx010/Desktop/blendimgtest/'+ i +'_2.png')
         sys.stdout.write('\r>> Converting image %d/%d' % (int(i), 119))
         sys.stdout.flush()
     print '\r'
@@ -21,8 +24,3 @@ def blend_two_images():
 
 if __name__ == '__main__':
     blend_two_images()
-
-修改img_add、label_add。
-
-range(3)中的3视图片数而定。
-
